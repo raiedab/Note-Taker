8c4d303fa525ec312c474c504f0b25d6cc2d1246
raied Abdelgani <raiedabdelgani@gmail.com>
2023-03-06 10:11:07 -0500

    fixed port

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded ( { 
-    extend: true}));
+app.use(express.urlencoded ({extend: true}));
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/notes', require('./Routes/api'))(app);
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 
-
-
-
-
-
-app.listen(PORT, () =>
-  console.log(`Server listening on http://localhost:${PORT}`)
+app.listen(port, () =>
+  console.log(`Server listening on http://localhost:${port}`)
 );
